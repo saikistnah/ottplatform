0ad2366d36f2c2d8a73de1c22db548e7db326baf
Saikrishna Gudla <saikrishna.gudla@gmail.com>
2020-08-01 20:44:30 +0530

    adding files

---
+++ src/main/java/com/sai/ottplatform/OttplatformApplication.java
@@ -0,0 +1,13 @@
+package com.sai.ottplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OttplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OttplatformApplication.class, args);
+	}
+
+}
